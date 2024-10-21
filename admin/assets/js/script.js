cdcb05b7a5ed209971c29ccd243c9fc2ad98192e
Yasir Akhtar <yasiralam662@gmail.com>
2024-10-21 11:44:11 +0530

    Dashboard Started !

---
+++ admin/assets/js/script.js
@@ -0,0 +1,22 @@
+var adminNavLinkBox = document.getElementById('adminNavLinkBox');
+var adminHeaderLinks = document.getElementById('adminHeaderLinks');
+var adminLogoutBtn = document.getElementById('adminLogoutBtn');
+
+adminHeaderLinks.addEventListener("click", function() {
+    if(adminNavLinkBox.style.display == "flex"){
+        adminNavLinkBox.style.display = "none";
+    }
+    else{
+        adminNavLinkBox.style.display = "flex";
+    }
+});
+
+// showAdminProfileLinks();
+
+
+adminLogoutBtn.addEventListener("click", function () {
+
+    alert("Storage Data Removing ....")
+
+    window.location.href = "/admin"
+});
